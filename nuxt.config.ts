b8e69207555898d296dbdc5fbf6e4ff580de81d0
Дмитрий Мартынов <dmitrijmartynov@MacBook-Air-Dmitrij.local>
2024-10-21 22:29:14 +0300

    reset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,4 @@
+import Aura from '@primevue/themes/aura';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
@@ -6,5 +7,23 @@ export default defineNuxtConfig({
     '@pinia/nuxt', // needed
     '@pinia-plugin-persistedstate/nuxt',
     '@nuxtjs/tailwindcss',
-  ]
+    '@primevue/nuxt-module',
+  ],
+  primevue: {
+    
+    autoImport: true,
+    
+    options: {
+        ripple: true,
+        inputVariant: 'filled',
+        theme: {
+            preset: Aura,
+            options: {
+                prefix: 'p',
+                darkModeSelector: 'system',
+                cssLayer: false
+            }
+        }
+    }
+}
 })
---
+++ pages/admin/login.vue
@@ -0,0 +1,38 @@
+<template>
+
+
+
+
+<InputGroup>
+    <InputGroupAddon>
+        <i class="pi pi-user"></i>
+    </InputGroupAddon>
+    <FloatLabel>
+        <InputText id="over_label" v-model="value1" />
+        <label for="over_label">Over Label</label>
+    </FloatLabel>
+</InputGroup>
+
+
+
+
+
+
+  </template>
+  
+  <script setup>
+import InputGroup from 'primevue/inputgroup';
+import InputGroupAddon from 'primevue/inputgroupaddon';
+  defineProps({
+    type: Object,
+    required: true
+  })
+
+
+  
+  
+  
+  
+  
+  
+  </script>
