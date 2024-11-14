ca87c653dafd7adc1bc6ff45d8606f6bd8290e33
Dexone <dm346@mail.ru>
2024-11-14 11:27:41 +0300

    tst

---
+++ pages/test.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    {{ data.existingProduct }}
+
+    <br />
+    <input type="email" v-model="email" /> <br />
+    <input type="password" v-model="password" /> <br />
+    <button @click="logintest">Войти</button> <br />
+    <button @click="addtocard">addtocard</button>
+  </div>
+</template>
+
+<script setup>
+const { data } = useFetch("http://194.58.121.72:3000/products?sortBy=power");
+
+const email = ref("test@mail.ru");
+const password = ref("12345678");
+
+async function logintest() {
+  const res = await $fetch('/api/login', {
+    method: "POST",
+    body: {
+      email: email.value,
+      password: password.value,
+    },
+  });
+
+console.log(res)
+
+  // console.log(data.value.access_token);
+  // localStorage.setItem("token", data.value.access_token);
+}
+
+async function addtocard() {
+  const { data } = await $fetch("/api/cart/add", {
+    method: "POST",
+    body: {
+      email: email.value,
+      productId: "26e1144a-920d-45b6-85ab-2fb87603e5e2",
+      quantity: 1,
+    },
+    headers: {
+      Authorization: localStorage.getItem("token"),
+    },
+  });
+}
+</script>
